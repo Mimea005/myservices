29074b73079e360a73838cf2ac8e106e0640a853
mimea005 <30933848+Mimea005@users.noreply.github.com>
2025-03-29 20:56:53 +0000

    Add config variables needed to access coolify api

--- common/config.go
+++ common/config.go
@@ -7,16 +7,22 @@ import (
 type serverConfig struct {
 	BindAddress string
 	LogFilters Filters
+	CoolifyURL string
+	CoolifyToken string
 }
 
 // Runtime configuration variables. Currently set as arguments
 var Config = serverConfig{
 	BindAddress: "",
 	LogFilters: []string{"handler", "service", "router"},
+	CoolifyURL: "",
+	CoolifyToken: "",
 }
 
 func init() {
 	flag.Var(&Config.LogFilters, "filter", "filter the log by category")
 	flag.StringVar(&Config.BindAddress, "bind", ":8080", "address to bind to")
+	flag.StringVar(&Config.CoolifyURL, "coolify-url", "", "coolify url")
+	flag.StringVar(&Config.CoolifyToken, "coolify-token", "", "Bearer token for coolify")
 }
 
